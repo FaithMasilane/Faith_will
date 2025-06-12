aab16914f0ac6866f959ce86ebc7a7205175e8fb
FaithMasilane <faithmasilane235@gmail.com>
2025-06-12 09:02:04 +0200

    faith will

---
+++ js/firebaseConfig.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyCIRU3nbvXDprTGqPH68-Rwp7aAH4et3v0",
+  authDomain: "mywill-app.firebaseapp.com",
+  databaseURL: "https://mywill-app-default-rtdb.firebaseio.com",
+  projectId: "mywill-app",
+  storageBucket: "mywill-app.appspot.com",
+  messagingSenderId: "243529229541",
+  appId: "1:243529229541:web:5388eedb26517353c7a00c",
+  measurementId: "G-VHHX0WYHPF"
+  };
+  firebase.initializeApp(config);
